d029ceb2b5dec9df108b3ad3a51c4b583b5e23a8
Marbin Ramírez Chávez <mramirez@inteligogroup.com>
2023-09-05 12:54:57 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using Siagie.Matricula.Migracion.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
